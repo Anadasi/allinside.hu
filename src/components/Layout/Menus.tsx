680fd1c34bbdc7084d3c21b7794f353696ef139d
Anadasi <nadasi.andras@gmail.com>
2026-01-30 13:10:07 +0100

    menü

--- src/components/Layout/Menus.tsx
+++ src/components/Layout/Menus.tsx
@@ -3,25 +3,30 @@ export const menus = [
     id: "home",
     title: "Főoldal",
     href:  "/",
+    isDropdown: false,
   },
   {
     id: "bemutatkozas",
     title: "Bemutatkozás",
     href:  "/doctors/profile/",
+    isDropdown: false,
   },
   {
     id: "gyakori",
     title: "Gyakori kérdések",
     href:  "/faq/",
+    isDropdown: false,
   },
   {
     id: "tovabbi",
     title: "További kezelések",
     href: "https://www.drmajzikerno.hu/",
+    isDropdown: false,
   },
   {
     id: "contact",
     title: "Kapcsolat",
     href: "/contact-us/",
+    isDropdown: false,
   },
 ];
