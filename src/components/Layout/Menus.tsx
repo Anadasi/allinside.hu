67e04f564edada04a14dedadab37284e669e4434
Anadasi <nadasi.andras@gmail.com>
2026-01-30 13:18:40 +0100

    menu structure fix

--- src/components/Layout/Menus.tsx
+++ src/components/Layout/Menus.tsx
@@ -4,34 +4,44 @@ export const menus = [
     title: "Főoldal",
     href:  "/",
     isDropdown: false,
-    dropdownItems: [],
+    dropdownItems: [
+    { title: "", href: "" } 
+  ],
   },
   {
     id: "bemutatkozas",
     title: "Bemutatkozás",
     href:  "/doctors/profile/",
     isDropdown: false,
-    dropdownItems: [],
+  dropdownItems: [
+    { title: "", href: "" } 
+  ],
   },
   {
     id: "gyakori",
     title: "Gyakori kérdések",
     href:  "/faq/",
     isDropdown: false,
-    dropdownItems: [],
+dropdownItems: [
+    { title: "", href: "" } 
+  ],
   },
   {
     id: "tovabbi",
     title: "További kezelések",
     href: "https://www.drmajzikerno.hu/",
     isDropdown: false,
-    dropdownItems: [],
+dropdownItems: [
+    { title: "", href: "" } 
+  ],
   },
   {
     id: "contact",
     title: "Kapcsolat",
     href: "/contact-us/",
     isDropdown: false,
-    dropdownItems: [],
+dropdownItems: [
+    { title: "", href: "" } 
+  ],
   },
 ];
