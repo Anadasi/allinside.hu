51b99cc3707f65eee8ada9fe7e628a4e458dc93a
Anadasi <nadasi.andras@gmail.com>
2026-02-20 12:16:33 +0100

    SEO frissítés

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Metadata } from 'next'; // 1. Add hozzá ezt az importot
 import Feedbacks from "@/components/Common/Feedbacks";
 import HowItWorks from "@/components/Common/HowItWorks";
 import OurDoctors from "@/components/Common/OurDoctors";
@@ -12,35 +13,38 @@ import OurBlog from "@/components/Common/OurBlog";
 import Cta from "@/components/Common/Cta";
 import Navbar from "@/components/Layout/Navbar";
 
+// 2. Szúrd be ezt a blokkot
+export const metadata: Metadata = {
+  title: 'Dr. Majzik Ernő, sportsebész sepcialista ! AllInside.hu',
+  description: 'All-inside keresztszalag műtét: A modern sportsebészet legfejlettebb megoldása',
+  keywords: ['egészségügy', 'keresztszalag', 'allinside', 'térdműtét', 'térd'],
+  openGraph: {
+    title: 'Dr. Majzik Ernő, sportsebész sepcialista ! AllInside.hu',
+    description: 'All-inside keresztszalag műtét: A modern sportsebészet legfejlettebb megoldása',
+    type: 'website',
+    url: 'https://allinside.hu/',
+  },
+};
+
 export default function Home() {
   return (
     <>
       <Navbar />
-
       <HeroBanner />
 
-<div id="roviden">
-  <Benefits />
-</div>
+      <div id="roviden">
+        <Benefits />
+      </div>
 
       <AboutUs />
-
       <OurServices />
-
       {/* <OurDoctors /> */}
-
       <HowItWorks />
-
       <Feedbacks />
-
       <PricingPlan />
-
       <FrequentlyAskedQuestions />
-
       {/* <DownloadOurApp /> */}
-
       {/* <OurBlog /> */}
-
       <Cta />
     </>
   );
