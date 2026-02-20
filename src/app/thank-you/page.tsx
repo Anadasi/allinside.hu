798911a78757a7eff34b19b7bbeff49af2e20e31
Anadasi <nadasi.andras@gmail.com>
2026-02-20 13:01:15 +0100

    leírás

--- src/app/thank-you/page.tsx
+++ src/app/thank-you/page.tsx
@@ -5,10 +5,10 @@ export default function ThankYouPage() {
     <div className="profile-authentication-area pt-140">
       <div className="container">
         <div className="profile-authentication-inner" style={{ textAlign: "center" }}>
-          <h2>Thank You</h2>
-          <p>Your submission has been received. We’ll be in touch soon.</p>
+          <h2>Köszönjük szépen!</h2>
+          <p>Üzenetét sikeresen megkaptuk!</p>
           <div style={{ marginTop: 24 }}>
-            <Link className="link-btn" href="/">Return Home</Link>
+            <Link className="link-btn" href="/">Vissza a főoldalra</Link>
           </div>
         </div>
       </div>
